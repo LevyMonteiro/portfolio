64079c76e80136d3ddfdf076169b19b1a29ae6ee
Levy Monteiro <levymonteiro268@gmail.com>
2024-02-14 19:42:39 -0300

    fix ErrorPage errors

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,23 @@
 import { useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
-  const error = useRouteError();
+  const error = useRouteError() as Error;
+
+  if (!error) {
+    return (
+      <div
+        id='error-page'
+        className='flex flex-col justify-center items-center h-screen w-screen'
+      >
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>An unknown error occurred.</i>
+        </p>
+      </div>
+    );
+  }
+
   console.error(error);
 
   return (
@@ -12,7 +28,7 @@ export default function ErrorPage() {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error.message}</i>
       </p>
     </div>
   );
