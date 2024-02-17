27cc9c9b2d50202c41672afe8289f66789ca972d
Levy Monteiro <levymonteiro268@gmail.com>
2024-02-17 20:14:18 -0300

    create Contact section responsive design

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -54,32 +54,32 @@ export default function Contact() {
       id='contact'
       className={`${
         theme === 'light' ? 'bg-violet-800' : 'bg-neutral-950'
-      } w-screen min-h-screen flex justify-center items-center py-24 text-white`}
+      } w-screen min-h-screen flex justify-center items-center py-24 xl:px-12 text-white`}
     >
       <div
         id='wrapper'
-        className='grid grid-cols-2 justify-center items-center max-w-[1184px] gap-12'
+        className='grid grid-cols-1 xl:grid-cols-2 justify-center items-center max-w-[1184px] w-4/5 xl:w-auto gap-12'
       >
         <div
           id='intro'
           className='flex flex-col gap-16 h-full justify-between pb-[68px]'
         >
-          <h2 className='text-white text-4xl font-poppins font-semibold'>
+          <h2 className='text-white text-4xl font-poppins font-semibold text-center xl:text-left'>
             Say Hello!
           </h2>
 
           <div id='intro-content' className='flex flex-col gap-5 text-pretty'>
-            <p className='opacity-85 text-lg'>
+            <p className='opacity-85 text-lg text-center xl:text-left'>
               Feel free to reach out if you have any questions or just want to
               say hello.
             </p>
 
-            <p className='opacity-85 text-lg'>
+            <p className='opacity-85 text-lg text-center xl:text-left'>
               Whether you're interested in making connections, sharing
               knowledge, discussing new projects, or exploring opportunities.
             </p>
 
-            <p className='opacity-85 text-lg'>
+            <p className='opacity-85 text-lg text-center xl:text-left'>
               I'm here to chat and ready to respond as soon as possible.
             </p>
           </div>
@@ -91,7 +91,7 @@ export default function Contact() {
         </div>
 
         <div id='form' className='flex flex-col gap-6 h-full'>
-          <h2 className='text-white text-4xl mb-2 font-poppins font-semibold'>
+          <h2 className='text-white text-4xl mb-2 font-poppins font-semibold text-center xl:text-left'>
             Get in Touch
           </h2>
 
@@ -171,7 +171,7 @@ export default function Contact() {
                 theme === 'light'
                   ? 'hover:bg-orange-500'
                   : 'border-violet-800 bg-violet-800 hover:bg-neutral-950 text-neutral-200 hover:text-violet-600 duration-300'
-              } w-min px-6 py-3 rounded-sm border-[2px] flex gap-1 text-lg items-center transition ease-linear`}
+              } w-full md:w-min px-6 py-3 rounded-sm border-[2px] flex gap-1 text-lg justify-center items-center transition ease-linear`}
             >
               <Send className='size-[22px]' />
               Submit
