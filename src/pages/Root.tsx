2ad48af037ff06eccbe1c30574a82d2ad5c89d32
Levy Monteiro <levymonteiro268@gmail.com>
2024-05-28 22:01:52 -0300

    create scroll animation

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -4,10 +4,17 @@ import Projects from '../components/sections/Projects';
 import Contact from '../components/sections/Contact';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
+import { motion, useScroll } from 'framer-motion';
 
 export default function Root() {
+  const { scrollYProgress } = useScroll();
+
   return (
     <div>
+      <motion.div
+        style={{ scaleX: scrollYProgress }}
+        className='progress-bar'
+      />
       <Nav currentPage='root' />
       <main>
         <Home />
